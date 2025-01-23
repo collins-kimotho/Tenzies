463757757b1d17f146586873fd1511a189d932fc
collins-kimotho <collinskimotho16@gmail.com>
2025-01-23 09:01:09 +0300

    confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import React from "react"
 import Die from "./Die"
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { nanoid } from "nanoid"
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
+import { useEffect } from "react"
 
 
 
 export default function App() {
-  const [dice, setDice] = useState(generateAllNewDice())
+  const [dice, setDice] = useState(() => generateAllNewDice())
+  const buttonRef = useRef(null)
+
+  const gameWon = dice.every(die => die.isHeld) &&
+                  dice.every(die => die.value === dice[0].value)
+
+  useEffect(() => {
+    if (gameWon) {
+        buttonRef.current.focus()
+    }
+}, [gameWon])
 
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
       .map(() => ({
-        value: Math.ceil(Math.random()*6),
+        // value: Math.ceil(Math.random()*6),
+        value: 5,
         isHeld: false,
         id: nanoid()
       }))
@@ -21,7 +35,7 @@ export default function App() {
   // property on the object in the array that was clicked
   // based on the 'id' prop passed into the function.
   function hold(id) {
-    console.log("hold function called with id: ", id)
+    
     setDice(oldDice => {
         return oldDice.map(die => {
             return die.id === id ?
@@ -30,6 +44,10 @@ export default function App() {
         })
     })
 }
+
+  function reset(){
+    setDice(generateAllNewDice())
+  }
 
 
   const diceElements = dice.map((dieObj) => (
@@ -44,24 +62,42 @@ export default function App() {
   ))
 
   function rollDice(){
-    setDice(generateAllNewDice())
+    if (!gameWon) {
+      setDice((prevDice) => prevDice.map((die) => die.isHeld ? die : {...die, value: Math.ceil(Math.random()*6)}))
+    } else {
+      reset( )
+    }
+    
   }
 
 
 
 
   return (
+    
     <main className="h-screen flex flex-col items-center justify-centre w-screen m-auto bg-gray-100  overflow-hidden p-6 ">
-      <div className="grid grid-cols-5 gap-4 p-4 rounded-lg items-center max-h-96 ">
-       {diceElements}
-       
+      {gameWon && <Confetti className="w-full h-full"/>}
+      <div className="flex flex-col bg-stone-300 items-center justify-center my-8 p-2 md:p-8 rounded-xl h-full">
+        <h1 className="font-bold text-3xl">Tenzies</h1>
+        <p className="text-xl text-center my-4">Roll untill all the dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className="grid grid-cols-5 gap-4 p-4 rounded-lg items-center max-h-96 ">
+        {diceElements}
+        
+        </div> 
+        <button
+          className="m-4 bg-blue-700/100 py-4 px-10 text-lg font-bold text-white rounded-md cursor-pointer hover:opacity-75"
+          onClick={rollDice}
+          ref={buttonRef}
+        >
+          {gameWon ? "New Game" : "Roll"}
+        </button>
+        <button className="my-4 bg-red-700/100 py-4 px-10 text-lf font-bold text-white rounded-md cursor-pointer hover:opacity-75"
+          onClick={reset}
+        >
+          Reset
+        </button>
       </div>
-      <button
-        className="m-4 bg-blue-700/100 py-4 px-10 text-lg font-bold text-white rounded-md cursor-pointer hover:opacity-75"
-        onClick={rollDice}
-      >
-        Roll
-      </button>
+     
       
       
       
--- src/Die.jsx
+++ src/Die.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Die = (props) => {
   return (
     <button
-      className={`bg-green h-14 w-14 rounded-lg border-0 text-lg font-bold shadow-lg cursor-pointer ${props.isHeld ? "bg-green-400" : "bg-white"}`}
+      className={`bg-green-400 md:h-14 md:w-14 h-12 w-12 rounded-lg border-0 text-lg font-bold shadow-lg cursor-pointer ${props.isHeld ? "bg-green-400" : "bg-white"}`}
       onClick={() => props.hold(props.id)}
+      pressed={props.isHeld}
+      aria-label={`Die with a value of ${props.value}, ${props.isHeld ? "Held" : "Not held"}`}
     >
       {props.value}
     </button>
